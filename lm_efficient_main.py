859ee43a40710d34cf214b111e0aef3691ee677b
RonaldsZakovskis <ronaldsz2000@gmail.com>
2020-09-21 10:21:25 +0300

    Hidden units now picked automatically based on number of parameters

--- lm_efficient_main.py
+++ lm_efficient_main.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import time
+import math
 from datetime import datetime  # We'll use this to dynamically generate training event names
 
 from sklearn.utils import shuffle
@@ -31,6 +32,7 @@ batch_size = 64  # Enwik8 – 64. PTB character-level 128?
 num_epochs = 1000000  # 5! We can code this to go infinity, but I see no point, we won't wait 1 million epochs anyway
 break_epochs_no_gain = 3  # If validation bpc doesn't get lower, after how many epochs we should break (-1 -> disabled)
 hidden_units = 256 * 7
+number_of_parameters = 24000000  # 24 million
 embedding_size = 256
 learning_rate = 0.0005  # At 0,001 LSTM and GRU explodes a bit, and at 0.0001 Mogrifier LSTM can't learn, so 0,0005!
 output_keep_prob = 0.9
@@ -377,6 +379,77 @@ class RNN_LM_Model:
             print(f"Final testing stats | Loss: {total_loss}, Perplexity: {total_perplexity}, BPC: {total_bpc}, Accuracy: {total_accuracy}, TimeSpent: {time.time() - start_time}")
 
 
+def find_optimal_hidden_units():
+    # Inspired from https://github.com/deepmind/lamb/blob/master/lamb/lamb_flags.py
+    # They made a version that takes in a config file, which might be more useful to some people
+    print(f"Searching for the largest possible hidden unit count"
+          f",  which has <= {number_of_parameters} trainable parameters!")
+
+    global hidden_units
+
+    # If there wasn't given correct number of total parameters, then just use the given hidden units
+    if number_of_parameters < 1:
+        return hidden_units
+
+    # If code goes this far, we don't care about the value in hidden_units variable anymore
+    # , we will change it after it returns something
+
+    def calculate_num_params_given_hidden_units(units):
+        global hidden_units
+        hidden_units = units
+        test_model = RNN_LM_Model()
+
+        # Get the number of paramaters in the current model
+        trainable_variables = tf.trainable_variables()
+        variable_count = 0
+        for variable in trainable_variables:
+            variable_count += np.product(variable.get_shape().as_list())
+
+        # These don't seem to be necessary, but they might help if there are some problems with memory
+        tf.keras.backend.clear_session()
+        del test_model
+
+        return variable_count
+
+    def is_good(hidden_count):
+        m = calculate_num_params_given_hidden_units(hidden_count)
+        correct = (m <= number_of_parameters)
+        if m is None:
+            print(f"Hidden units = {hidden_count}, number of trainable parameters = None BAD")
+        elif correct:
+            print(f"Hidden units = {hidden_count}, number of trainable parameters = {m} GOOD")
+        else:
+            print(f"Hidden units = {hidden_count}, number of trainable parameters = {m} BAD")
+        return correct, m
+
+    # Double the size until it's too large.
+    previous_hidden_size = 1
+    hidden_size = 1
+    good, n = is_good(hidden_size)
+    while good:
+        previous_hidden_size = hidden_size
+        hidden_size = max(hidden_size + 1, int(hidden_size * math.sqrt(1.2 * number_of_parameters / n)))
+        good, n = is_good(hidden_size)
+
+    # Find the real answer in the range – [previous_hidden_size, hidden_size] range
+    def find_answer(lower, upper):
+        while lower < upper - 1:  # While the difference is bigger than 1
+            # The number of parameters is likely to be at least quadratic in
+            # hidden_size. Find the middle point in log space.
+            # math.exp does e^x, where x is given.
+            # math.log does ln(x) aka e^y=x
+            middle = int(math.exp((math.log(upper) + math.log(lower)) / 2))
+            # The middle has to be 1 larger than the bottom limit or 1 smaller then the upper limit
+            middle = min(max(middle, lower + 1), upper - 1)
+            if is_good(middle)[0]:
+                lower = middle
+            else:
+                upper = middle
+        return lower
+
+    return find_answer(previous_hidden_size, hidden_size)
+
+
 if __name__ == '__main__':  # Main function
     TRAIN_DATA, VALID_DATA, TEST_DATA, vocabulary_size = load_data(data_set_name)  # Load data set
 
@@ -386,6 +459,8 @@ if __name__ == '__main__':  # Main function
     VALID_DATA = VALID_DATA[:len(VALID_DATA) // 5]
     TEST_DATA = TEST_DATA[:len(TEST_DATA) // 5]
     '''
+
+    hidden_units = find_optimal_hidden_units()
 
     model = RNN_LM_Model()  # Create the model
 
